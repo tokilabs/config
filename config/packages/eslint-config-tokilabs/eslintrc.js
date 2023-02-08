d308920a0ff844131ce22d3cbda18e96d67d6878
Gabriela Sanches <gabi@toki.life>
2023-02-08 15:37:59 -0300

    config: eslint.js and its package.json

---
+++ config/packages/eslint-config-tokilabs/eslintrc.js
@@ -0,0 +1,79 @@
+module.exports = {
+    plugins: ['@typescript-eslint'],
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: '',
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
+    },
+    ignorePatterns: ['node_modules', 'build', 'notes', 'build-*'],
+    globals: {
+      h: 'readable',
+      figma: 'readable',
+      __html__: 'readable',
+      describe: 'readable',
+      it: 'readable',
+      expect: 'readable',
+      cy: 'readable',
+      jest: 'readable',
+    },
+    env: {
+      browser: true,
+      es2021: true,
+      es2017: true,
+      node: true,
+    },
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'plugin:@typescript-eslint/strict',
+      'prettier',
+      './eslint.json',
+      '../tsconfig/base.json'
+    ],
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+      '@typescript-eslint/unified-signatures': 0,
+      "turbo/no-undeclared-env-vars": "error",
+		  "@typescript-eslint/array-type": [
+			"warn",
+			{default: "array-simple", readonly: "array-simple"}
+		  ],
+      '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '_|h' }],
+      'import/prefer-default-export': 0,
+      'import/extensions': 0,
+      'no-await-in-loop': 0,
+      'no-param-reassign': 0,
+      'class-methods-use-this': 0,
+      'no-restricted-globals': 0,
+      'max-len': 0,
+      'react/function-component-definition': 0,
+      'react/require-default-props': 0,
+      'react/jsx-props-no-spreading': 0,
+      'no-prototype-builtins': 0,
+      'no-async-promise-executor': 0,
+      'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+      '@typescript-eslint/ban-types': [
+        2,
+        {
+          types: {
+            object: {
+              message:
+                'The `Object` type actually means "any non-nullish value", so it is marginally better than `unknown`.\n- If you want a type meaning "any object", you probably want `Record<string, unknown>` instead.\n- If you want a type meaning "any value", you probably want `unknown` instead.',
+            },
+          },
+          extendDefaults: true,
+        },
+      ],
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-shadow': 1,
+      
+    },
+    settings: {
+      jest: {
+        version: 27,
+      },
+    },
+  };
