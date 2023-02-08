6862e00fdc42fafcf7dc734d9918dcabf41ba1fe
Gabriela Sanches <gabi@toki.life>
2023-02-08 19:11:05 -0300

    refactor: right folder structure

--- packages/prettier-config-tokilabs/prettierrc.js
+++ packages/prettier-config-tokilabs/prettierrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-    printWidth: 80,
-    tabWidth: 2,
-    singleQuote: true,
-    trailingComma: 'es5',
+  extends: ['..//tsconfig/base.json'],
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'es5',
 	arrowParens: "always",
 	useTabs: true,
 	semi: true,
-	bracketSpacing: false
-  };
+	bracketSpacing: false 
+};
